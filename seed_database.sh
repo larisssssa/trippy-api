4a9cf45ddf3328e9bbc442352a76245b4b299ed7
Larissa Ferreira <larissadferr@gmail.com>
2026-05-06 11:52:33 -0500

    seed_database: added loaddata for new models

--- seed_database.sh
+++ seed_database.sh
@@ -7,3 +7,8 @@ python manage.py makemigrations trippyapi
 python manage.py migrate trippyapi
 python manage.py loaddata users
 python manage.py loaddata tokens
+python manage.py loaddata categories
+python manage.py loaddata trip
+python manage.py loaddata attraction
+python manage.py loaddata tripuser
+python manage.py loaddata tripattraction
