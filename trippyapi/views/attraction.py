5a693b5abbfdfbeac0503abcf76522fff6a90dbc
Larissa Ferreira <larissadferr@gmail.com>
2026-05-07 15:03:32 -0500

    views/attraction: added destroy function

--- trippyapi/views/attraction.py
+++ trippyapi/views/attraction.py
@@ -62,17 +62,17 @@ class Attractions(ViewSet):
         return Response(None, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
-        """Handle DELETE requests for a single item
+        """Handle DELETE requests for a single attraction
 
         Returns:
             Response -- 200, 404, or 500 status code
         """
         try:
-            void = Void.objects.get(pk=pk)
-            void.delete()
+            attraction = Attraction.objects.get(pk=pk)
+            attraction.delete()
             return Response(None, status=status.HTTP_204_NO_CONTENT)
 
-        except Void.DoesNotExist as ex:
+        except Attraction.DoesNotExist as ex:
             return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
 
         except Exception as ex:
