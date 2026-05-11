997a21e964f483a2a08fed09157b8f23ae1dc361
Larissa Ferreira <larissadferr@gmail.com>
2026-05-11 13:40:37 -0500

    models/attraction: added city and country fields

--- trippyapi/models/attraction.py
+++ trippyapi/models/attraction.py
@@ -5,5 +5,7 @@ from .category import Category
 class Attraction(models.Model):
     name = models.CharField()
     description = models.TextField()
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
     imageurl = models.CharField()
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
